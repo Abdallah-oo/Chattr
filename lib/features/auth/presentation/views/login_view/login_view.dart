bc405c5056cd9e27b79280b678535f25adfcc7f3
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-13 00:24:07 +0300

    feat: handle auth state changes in login listener

--- lib/features/auth/presentation/views/login_view/login_view.dart
+++ lib/features/auth/presentation/views/login_view/login_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:messenger_clone0/core/helpers/snack_bar.dart';
+import 'package:messenger_clone0/core/routing/routes.dart';
+import 'package:messenger_clone0/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:messenger_clone0/features/auth/presentation/views/login_view/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -8,7 +13,18 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(body: LoginViewBody()),
+      child: BlocListener<AuthCubit, AuthState>(
+        listener: (context, state) {
+           if (state.status == AuthStatus.failure) {
+            CustomSnackBar.error(context, state.errorMessage ?? '');
+          }
+          if (state.status == AuthStatus.success) {
+            context.pushReplacement(Routes.root);
+            CustomSnackBar.success(context, 'Login Successfully');
+          }
+        },
+        child: Scaffold(body: LoginViewBody()),
+      ),
     );
   }
 }
