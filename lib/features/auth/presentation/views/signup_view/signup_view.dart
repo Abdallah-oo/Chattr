63490f524c8803413ee7468321c354d1c574e3a3
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-13 00:24:40 +0300

    feat: handle auth state changes in signup listener

--- lib/features/auth/presentation/views/signup_view/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view/signup_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:messenger_clone0/core/helpers/snack_bar.dart';
+import 'package:messenger_clone0/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:messenger_clone0/features/auth/presentation/views/signup_view/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -8,7 +12,21 @@ class SignupView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(body: SignupViewBody()),
+      child: BlocListener<AuthCubit, AuthState>(
+        listener: (context, state) {
+         if (state.status == AuthStatus.failure) {
+            CustomSnackBar.error(context, state.errorMessage ?? '');
+          }
+          if (state.status == AuthStatus.success) {
+            CustomSnackBar.success(
+              context,
+              'Successfully Registered , you can login now',
+            );
+            context.pop();
+          }
+        },
+        child: Scaffold(body: SignupViewBody()),
+      ),
     );
   }
 }
