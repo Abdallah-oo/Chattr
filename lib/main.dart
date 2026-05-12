87ab8cf126c5adffc9070405df7e01bfcbdc17e4
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-13 00:25:37 +0300

    chore: initialize hive and dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:messenger_clone0/core/services/hive/hive_services.dart';
 import 'package:messenger_clone0/core/services/supabase/supabase_constants.dart';
+import 'package:messenger_clone0/core/utils/di/get_it.dart';
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:messenger_clone0/messenger_clone_app.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -9,6 +10,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   // Initialize Supabase
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  setUpGetIt();
     await Supabase.initialize(
     url: SupabaseConstants.url,
     anonKey: SupabaseConstants.anonKey,
