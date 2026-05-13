3ffb2bbbfc840738c814a7eaee0d76e9fe516756
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-13 22:33:34 +0300

    feat : add fetch me method and  refactor save_users method

--- lib/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo.dart
+++ lib/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo.dart
@@ -4,9 +4,11 @@ import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class FetchContactsRepo {
-Future<Either<SupabaseError, List<Map<String, dynamic>>>>fetchAllContacts(
+  Future<Either<SupabaseError, List<Map<String, dynamic>>>> fetchAllContacts(
     List<String> ids,
-  );  Either<SupabaseError, RealtimeChannel> subscribeToUser(String userId);
+  );
+  Future<Either<SupabaseError, UserModel>> fetchMe(String myId);
+  Either<SupabaseError, RealtimeChannel> subscribeToUser(String userId);
   Future<Either<String, List<UserModel>>> getUsers();
   Future<Either<String, Unit>> saveUsers(List<UserModel> users);
   Future<Either<String, Unit>> saveUser(UserModel user);
--- lib/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo_impl.dart
+++ lib/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:messenger_clone0/core/services/hive/hive_services.dart';
+import 'package:messenger_clone0/core/services/supabase/supabase_crud_services.dart';
 import 'package:messenger_clone0/core/services/supabase/supabase_error.dart';
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:messenger_clone0/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo.dart';
@@ -7,9 +8,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class FetchContactsRepoImpl implements FetchContactsRepo {
   final SupabaseClient _client;
-  FetchContactsRepoImpl(this._client);
+  final SupabaseCrudServices _crud;
+  FetchContactsRepoImpl(this._client, this._crud);
 
- @override
+  @override
   Future<Either<SupabaseError, List<Map<String, dynamic>>>> fetchAllContacts(
     List<String> ids,
   ) async {
@@ -20,6 +22,16 @@ class FetchContactsRepoImpl implements FetchContactsRepo {
           .inFilter('id', ids);
 
       return right(result);
+    } catch (e) {
+      return left(SupabaseError(message: "$e"));
+    }
+  }
+
+  @override
+  Future<Either<SupabaseError, UserModel>> fetchMe(String myId) async {
+    try {
+      final myData = await _crud.getById(table: 'messenger_users', id: myId);
+      return right(UserModel.fromJson(myData));
     } catch (e) {
       return left(SupabaseError(message: "$e"));
     }
@@ -58,7 +70,9 @@ class FetchContactsRepoImpl implements FetchContactsRepo {
   @override
   Future<Either<String, Unit>> saveUsers(List<UserModel> users) async {
     try {
-      await HiveService.replaceUsers(users);
+      for (var u in users) {
+        await HiveService.saveUser(u);
+      }
       return right(unit);
     } catch (e) {
       return left('$e');
