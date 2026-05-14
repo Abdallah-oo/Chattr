1d18fc6e746ceb8897a1828b672e541a687b10b6
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-14 14:54:20 +0300

    refactor: improve root navigation bar structure

--- lib/root.dart
+++ lib/root.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:messenger_clone0/core/themes/app_colors.dart';
-
+import 'package:messenger_clone0/features/contacts/presentation/views/contacts_view.dart';
 
 class Root extends StatefulWidget {
   const Root({super.key});
@@ -14,13 +13,14 @@ class Root extends StatefulWidget {
 class _RootState extends State<Root> {
   int currentIndex = 0;
 
-  final List<Widget> pages = const [
-  
-  ];
+  final List<Widget> pages = const [ContactsView()];
 
   final List<_NavItem> items = const [
-    _NavItem(Icons.home, Icons.contact_page_rounded, 'contacts'),
-
+    _NavItem(
+      Icons.contact_page_rounded,
+      Icons.contact_page_outlined,
+      'contacts',
+    ),
   ];
 
   void onTap(int index) {
@@ -30,24 +30,22 @@ class _RootState extends State<Root> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            IndexedStack(index: currentIndex, children: pages),
-            Positioned(
-              bottom: 20,
-              left: 14,
-              right: 14,
-              child: _ModernNavBar(
-                currentIndex: currentIndex,
-                items: items,
-                onTap: onTap,
-              ),
+    return SafeArea(
+      child: Stack(
+        children: [
+          IndexedStack(index: currentIndex, children: pages),
+          Positioned(
+            bottom: 20,
+            left: 14,
+            right: 14,
+            child: 
+            _ModernNavBar(
+              currentIndex: currentIndex,
+              items: items,
+              onTap: onTap,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -67,11 +65,11 @@ class _ModernNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(30),
+      borderRadius: BorderRadius.circular(40),
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
         child: Container(
-          height: 68,
+          height: 65,
           decoration: BoxDecoration(
             color: Colors.white.withOpacity(0.05),
             borderRadius: BorderRadius.circular(30),
@@ -83,38 +81,19 @@ class _ModernNavBar extends StatelessWidget {
               final isSelected = index == currentIndex;
 
               return GestureDetector(
-                      onTap: () => onTap(index),
-                      behavior: HitTestBehavior.opaque,
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 250),
-                        curve: Curves.easeOut,
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            AnimatedContainer(
-                              duration: const Duration(milliseconds: 250),
-                              height: 4,
-                              width: isSelected ? 30 : 0,
-                              margin: const EdgeInsets.only(bottom: 6),
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF20B812),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            Icon(
-                              isSelected
-                                  ? items[index].activeIcon
-                                  : items[index].icon,
-                              color: isSelected
-                                  ? const Color(0xFF20B812)
-                                  : Colors.grey,
-                              size: 26,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
+                onTap: () => onTap(index),
+                behavior: HitTestBehavior.opaque,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 250),
+                  curve: Curves.easeOut,
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Icon(
+                    isSelected ? items[index].activeIcon : items[index].icon,
+                    color: isSelected ? AppColors.primary : Colors.grey,
+                    size: 28,
+                  ),
+                ),
+              );
             }),
           ),
         ),
