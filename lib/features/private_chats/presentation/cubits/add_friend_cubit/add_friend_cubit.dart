78dec017e4374b59eb7412fc2390d993bc54ed07
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-14 16:05:24 +0300

    feat: add add friend cubit and state management

---
+++ lib/features/private_chats/presentation/cubits/add_friend_cubit/add_friend_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:messenger_clone0/features/private_chats/data/repos/add_friend_repo.dart';
+part 'add_friend_state.dart';
+
+class AddFriendCubit extends Cubit<AddFriendState> {
+  AddFriendCubit(this._repo) : super(AddFriendInitial());
+  final AddFriendRepo _repo;
+
+  Future<void> addFriend({required String email}) async {
+    emit(AddFriendLoading());
+    final result = await _repo.addFriend(email);
+    result.fold(
+      (l) => emit(AddFriendFailure(errMessage: l.message)),
+      (r) => emit(AddFriendSuccess()),
+    );
+  }
+}
