b171ef451cd1fe09f6329d18967ea6b5d10d64f6
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-16 18:29:50 +0300

    refactor : refactor pick image ui

--- lib/core/widgets/image/ui/pick_image.dart
+++ lib/core/widgets/image/ui/pick_image.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:messenger_clone0/core/cubits/pick_image/pick_image_cubit.dart';
 import 'package:messenger_clone0/core/themes/app_colors.dart';
-import 'package:messenger_clone0/core/widgets/image/image_source_bottom.dart';
+import 'package:messenger_clone0/core/widgets/image/widgets/image_source_bottom.dart';
 const _kAvatarRadius = 45.0;
 const _kFallbackAvatarUrl =
     'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/default-avatar-profile-picture-male-icon.png';
