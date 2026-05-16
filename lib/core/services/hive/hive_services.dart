42865d2571c27c882c77cb8e713d8b9098e8f9ab
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-16 18:54:39 +0300

    feat: integrate private chats in hive services

--- lib/core/services/hive/hive_services.dart
+++ lib/core/services/hive/hive_services.dart
@@ -1,6 +1,8 @@
 import 'package:hive/hive.dart';
 import 'package:messenger_clone0/core/cache/users_cache.dart';
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
+import 'package:messenger_clone0/features/private_chats/data/models/private_chat_model.dart';
+import 'package:messenger_clone0/features/private_chats/data/models/private_message_model.dart';
 
 
 class HiveService {
@@ -34,6 +36,78 @@ class HiveService {
   static Future<List<UserModel>> getUsers() async {
     final box = Hive.box<UserModel>('users');
     return box.values.toList();
+  }
+
+  //--------------- PRIVATE CHATS ----------------
+  static Future<void> savePrivateChat(PrivateChatModel chat) async {
+    final box = Hive.box<PrivateChatModel>(privateChatsBoxName);
+    await box.put(chat.chatId, chat);
+  }
+
+  static Future<List<PrivateChatModel>> getPrivateChats() async {
+    final box = Hive.box<PrivateChatModel>(privateChatsBoxName);
+    return box.values.toList();
+  }
+
+  static Future<void> replacePrivateChats(List<PrivateChatModel> chats) async {
+    final box = Hive.box<PrivateChatModel>(privateChatsBoxName);
+    await box.clear();
+    for (var g in chats) {
+      await box.put(g.chatId, g);
+    }
+  }
+
+  static Future<void> clearChats() async {
+    final box = Hive.box<PrivateChatModel>(privateChatsBoxName);
+    await box.clear();
+  }
+
+ //----------------Private Messages ----------------
+   static Future<void> savePrivateMessage(PrivateMessageModel message) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    final key = message.messageId ?? message.tempId;
+    await box.put(key, message);
+  }
+
+  static Future<void> deletePrivateMessage(String key) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    await box.delete(key);
+  }
+
+  static Future<List<PrivateMessageModel>> getPrivateMessages(
+    String chatId, {
+    int limit = 30,
+  }) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    final messages = box.values.where((m) => m.chatId == chatId).toList();
+    messages.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+    if (messages.length <= limit) return messages;
+    return messages.sublist(messages.length - limit);
+  }
+
+  static Future<PrivateMessageModel?> getPrivateMessage(
+    String messageId,
+  ) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    return box.get(messageId);
+  }
+
+  static Future<void> savePrivateMessageLocalPath({
+    required String messageId,
+    required String localPath,
+  }) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    final msg = box.get(messageId);
+
+    if (msg == null) return;
+    await box.put(messageId, msg.copyWith(localPath: localPath));
+  }
+
+  /// جيب الـ local path
+  static Future<String?> getPrivateMessageLocalPath(String messageId) async {
+    final box = Hive.box<PrivateMessageModel>(privateMessageBoxName);
+    final msg = box.get(messageId);
+    return msg?.localPath;
   }
 
  
--- lib/core/services/hive/hive_type_ids.dart
+++ lib/core/services/hive/hive_type_ids.dart
@@ -3,5 +3,8 @@ class HiveTypeIds {
   static const users = 1;
   //private chats
   static const privateChats = 2;
+   static const privateMessages = 3;
+  static const privateMessageType = 4;
+  static const privateMessageStatus = 5;
   
 }
