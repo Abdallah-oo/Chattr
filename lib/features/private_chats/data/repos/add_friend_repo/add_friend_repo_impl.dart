760187ae6c493abe9d24abc5ee14151bd6ac16bc
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-16 18:34:45 +0300

    refactor : refactor add friend repository

--- lib/features/private_chats/data/repos/add_friend_repo/add_friend_repo_impl.dart
+++ lib/features/private_chats/data/repos/add_friend_repo/add_friend_repo_impl.dart
@@ -5,7 +5,7 @@ import 'package:messenger_clone0/core/services/supabase/supabase_crud_services.d
 import 'package:messenger_clone0/core/services/supabase/supabase_error.dart';
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:messenger_clone0/features/private_chats/data/models/private_chat_model.dart';
-import 'package:messenger_clone0/features/private_chats/data/repos/add_friend_repo.dart';
+import 'package:messenger_clone0/features/private_chats/data/repos/add_friend_repo/add_friend_repo.dart';
 
 class AddFriendRepoImpl implements AddFriendRepo {
   final SupabaseCrudServices _crud;
