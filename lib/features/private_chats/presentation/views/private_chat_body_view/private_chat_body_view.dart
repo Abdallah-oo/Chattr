5dcd60452f4b75795981ec54f962a84a5a0a52d4
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-18 19:33:11 +0300

    refactor: setup chat cubits and dependencies in PrivateChatBodyView

--- lib/features/private_chats/presentation/views/private_chat_body_view/private_chat_body_view.dart
+++ lib/features/private_chats/presentation/views/private_chat_body_view/private_chat_body_view.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:messenger_clone0/core/cubits/audio_cubit/audio_cubit.dart';
+import 'package:messenger_clone0/core/cubits/pick_image/pick_image_cubit.dart';
 import 'package:messenger_clone0/core/cubits/select_messages/select_messages_cubit.dart';
+import 'package:messenger_clone0/core/utils/di/get_it.dart';
 import 'package:messenger_clone0/core/widgets/custom_appbar.dart';
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:messenger_clone0/features/private_chats/data/models/private_message_model.dart';
+import 'package:messenger_clone0/features/private_chats/data/repos/send_private_message_repo/send_private_message_repo.dart';
+import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_chats_cubit/fetch_private_chats_cubit.dart';
 import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_messages_cubit/fetch_private_messages_cubit.dart';
+import 'package:messenger_clone0/features/private_chats/presentation/cubits/send_private_message_cubit/send_private_message_cubit.dart';
 import 'package:messenger_clone0/features/private_chats/presentation/views/private_chat_body_view/private_chat_body_view_body.dart';
 import 'package:messenger_clone0/features/private_chats/presentation/views/private_chats_view/widgets/online_status_widget.dart';
 
-class PrivateChatBodyView extends StatefulWidget {
+class PrivateChatBodyView extends StatelessWidget {
   const PrivateChatBodyView({
     super.key,
     required this.chatData,
@@ -19,14 +25,9 @@ class PrivateChatBodyView extends StatefulWidget {
   final dynamic chatData;
   final UserModel user;
 
-  @override
-  State<PrivateChatBodyView> createState() => _PrivateChatBodyViewState();
-}
-
-class _PrivateChatBodyViewState extends State<PrivateChatBodyView> {
-  void deletemessage(List<dynamic> selected) {
+  void deletemessage({required List<dynamic> selected,required BuildContext context}) {
     context.read<FetchPrivateMessagesCubit>().deletePrivateMessages(
-      chatId: widget.chatData.chatId!,
+      chatId: chatData.chatId!,
       messages: selected.cast<PrivateMessageModel>(),
     );
 
@@ -34,64 +35,69 @@ class _PrivateChatBodyViewState extends State<PrivateChatBodyView> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      context.read<FetchPrivateMessagesCubit>().loadInitialMessages(
-        chatId: widget.chatData.chatId!,
-      );
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(
-        appBar: CustomAppBar(
-          title: widget.chatData.friend.name ?? "",
-          titleItems: [OnlineStatusWidget(chatId: widget.chatData.chatId!)],
-          leading: GestureDetector(
-            onTap: () => context.pop(),
-            child: Icon(Icons.arrow_back_ios_new_rounded, size: 15),
-          ),
-          actions: [
-            BlocBuilder<SelectMessagesCubit, SelectMessagesState>(
-              builder: (context, state) {
-                final selectedmessages = context
-                    .read<SelectMessagesCubit>()
-                    .selectedMessages;
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => SelectMessagesCubit()),
+          BlocProvider.value(value: getIt<FetchPrivateMessagesCubit>()),
 
-                return selectedmessages.isNotEmpty
-                    ? Row(
-                        children: [
-                          context.read<SelectMessagesCubit>().containMedia()
-                              ? SizedBox.shrink()
-                              : InkWell(
-                                  onTap: () {
-                                    context
-                                        .read<SelectMessagesCubit>()
-                                        .copyMessages();
-                                  },
-                                  child: Icon(Icons.copy, size: 20),
-                                ),
-                          Gap(5),
-                          InkWell(
-                            onTap: () => deletemessage(selectedmessages),
-                            child: Icon(Icons.delete_outlined, size: 25),
-                          ),
-                          Gap(10),
-                        ],
-                      )
-                    : SizedBox.shrink();
-              },
+          BlocProvider.value(value: getIt<FetchPrivateChatsCubit>()),
+          BlocProvider(
+            create: (context) => SendPrivateMessageCubit(
+              fetchCubit: getIt<FetchPrivateMessagesCubit>(),
+              repo: getIt<SendPrivateMessageRepo>(),
             ),
-          ],
-        ),
-        body: PrivateChatBodyViewBody(
-          chatData: widget.chatData,
-          curruntUser: widget.user,
+          ),
+          BlocProvider(create: (context) => AudioCubit()),
+          BlocProvider(create: (context) => PickImageCubit()),
+        ],
+        child: Scaffold(
+          appBar: CustomAppBar(
+            title: chatData.friend.name ?? "",
+            titleItems: [OnlineStatusWidget(chatId: chatData.chatId!)],
+            leading: GestureDetector(
+              onTap: () => context.pop(),
+              child: Icon(Icons.arrow_back_ios_new_rounded, size: 15),
+            ),
+            actions: [
+              BlocBuilder<SelectMessagesCubit, SelectMessagesState>(
+                builder: (context, state) {
+                  final selectedmessages = context
+                      .read<SelectMessagesCubit>()
+                      .selectedMessages;
+
+                  return selectedmessages.isNotEmpty
+                      ? Row(
+                          children: [
+                            context.read<SelectMessagesCubit>().containMedia()
+                                ? SizedBox.shrink()
+                                : InkWell(
+                                    onTap: () {
+                                      context
+                                          .read<SelectMessagesCubit>()
+                                          .copyMessages();
+                                    },
+                                    child: Icon(Icons.copy, size: 20),
+                                  ),
+                            Gap(5),
+                            InkWell(
+                              onTap: () => deletemessage(selected: selectedmessages,context: context),
+                              child: Icon(Icons.delete_outlined, size: 25),
+                            ),
+                            Gap(10),
+                          ],
+                        )
+                      : SizedBox.shrink();
+                },
+              ),
+            ],
+          ),
+          body: PrivateChatBodyViewBody(
+            chatData: chatData,
+            curruntUser: user,
+          ),
         ),
       ),
     );
