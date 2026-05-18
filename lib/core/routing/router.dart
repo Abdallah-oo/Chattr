b6b826602a223bf7c862c4d776715a8296f1d864
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-18 18:46:50 +0300

    refactor: simplify private chat routing and cubit injection

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:messenger_clone0/core/cubits/audio_cubit/audio_cubit.dart';
 import 'package:messenger_clone0/core/cubits/download_image/download_image_cubit.dart';
 import 'package:messenger_clone0/core/cubits/fetch_current_user_data/fetch_current_user_data_cubit.dart';
 import 'package:messenger_clone0/core/cubits/pick_image/pick_image_cubit.dart';
-import 'package:messenger_clone0/core/cubits/select_messages/select_messages_cubit.dart';
 import 'package:messenger_clone0/core/routing/router_models.dart';
 import 'package:messenger_clone0/core/routing/routes.dart';
 import 'package:messenger_clone0/core/services/supabase/supabase_auth_services.dart';
@@ -18,12 +16,6 @@ import 'package:messenger_clone0/features/auth/presentation/views/login_view/log
 import 'package:messenger_clone0/features/auth/presentation/views/signup_view/signup_view.dart';
 import 'package:messenger_clone0/features/contacts/data/repos/fetch_contacts_repo/fetch_contacts_repo.dart';
 import 'package:messenger_clone0/features/contacts/presentation/cubits/fetch_contacts_cubit/fetch_contacts_cubit.dart';
-import 'package:messenger_clone0/features/private_chats/data/repos/fetch_private_chats_repo/fetch_private_chat_repo.dart';
-import 'package:messenger_clone0/features/private_chats/data/repos/fetch_private_messages_repo/fetch_private_messages_repo.dart';
-import 'package:messenger_clone0/features/private_chats/data/repos/send_private_message_repo/send_private_message_repo.dart';
-import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_chats_cubit/fetch_private_chats_cubit.dart';
-import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_messages_cubit/fetch_private_messages_cubit.dart';
-import 'package:messenger_clone0/features/private_chats/presentation/cubits/send_private_message_cubit/send_private_message_cubit.dart';
 import 'package:messenger_clone0/features/private_chats/presentation/views/private_chat_body_view/private_chat_body_view.dart';
 import 'package:messenger_clone0/root.dart';
 
@@ -55,25 +47,8 @@ abstract class AppRouter {
       GoRoute(
         path: Routes.root,
         builder: (context, state) {
-          final privateMessagesCubit = FetchPrivateMessagesCubit(
-            auth: getIt<AuthService>(),
-            client: getIt<SupabaseClientManager>(),
-            repo: getIt<FetchPrivateMessagesRepo>(),
-          );
-
-          final chatsCubit = FetchPrivateChatsCubit(
-            client: getIt<SupabaseClientManager>(),
-            fetchMessages: privateMessagesCubit,
-            repo: getIt<FetchPrivateChatRepo>(),
-          );
-
-          // اربط الـ messagesCubit بالـ chatsCubit عشان يحدّث الـ unread بعد markAllAsRead
-          privateMessagesCubit.setChatsCubit(chatsCubit);
-
           return MultiBlocProvider(
             providers: [
-              BlocProvider.value(value: privateMessagesCubit),
-              BlocProvider.value(value: chatsCubit..fetchPrivateChats()),
               BlocProvider(
                 create: (context) => FetchCurrentUserDataCubit(
                   auth: getIt<AuthService>(),
@@ -111,25 +86,9 @@ abstract class AppRouter {
         builder: (context, state) {
           final chatData = state.extra as PrivateChatParams;
 
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider(create: (context) => SelectMessagesCubit()),
-              BlocProvider.value(value: chatData.chatCubit),
-
-              BlocProvider.value(value: chatData.messagesCubit),
-              BlocProvider(
-                create: (context) => SendPrivateMessageCubit(
-                  fetchCubit: chatData.messagesCubit,
-                  repo: getIt<SendPrivateMessageRepo>(),
-                ),
-              ),
-              BlocProvider(create: (context) => AudioCubit()),
-              BlocProvider(create: (context) => PickImageCubit()),
-            ],
-            child: PrivateChatBodyView(
-              chatData: chatData.chatData,
-              user: chatData.curruntUser,
-            ),
+          return PrivateChatBodyView(
+            chatData: chatData.chatData,
+            user: chatData.curruntUser,
           );
         },
       ),
--- lib/core/routing/router_models.dart
+++ lib/core/routing/router_models.dart
@@ -1,21 +1,16 @@
 
 import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
 import 'package:messenger_clone0/features/private_chats/data/models/private_chat_model.dart';
-import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_chats_cubit/fetch_private_chats_cubit.dart';
-import 'package:messenger_clone0/features/private_chats/presentation/cubits/fetch_private_messages_cubit/fetch_private_messages_cubit.dart';
 
 
 //!private chats
 class PrivateChatParams {
   final PrivateChatModel chatData;
-  final FetchPrivateMessagesCubit messagesCubit;
-  final FetchPrivateChatsCubit chatCubit;
   final UserModel curruntUser;
   PrivateChatParams({
     required this.chatData,
     required this.curruntUser,
-    required this.messagesCubit,
-    required this.chatCubit,
+
   });
 }
 
