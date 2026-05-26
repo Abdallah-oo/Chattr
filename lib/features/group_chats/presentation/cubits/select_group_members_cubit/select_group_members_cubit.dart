727f3d94d4c108c203ff1a077c374b167f9dc2f4
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-26 17:49:03 +0300

    feat: implement select group members

---
+++ lib/features/group_chats/presentation/cubits/select_group_members_cubit/select_group_members_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:messenger_clone0/features/auth/data/models/user_model.dart';
+
+part 'select_group_members_state.dart';
+
+class SelectGroupMembersCubit extends Cubit<SelectGroupMembersState> {
+  SelectGroupMembersCubit() : super(SelectGroupMembersInitial());
+    List<UserModel> selectedMembers = [];
+  void addMembers({required UserModel user}) {
+    if (selectedMembers.contains(user)) {
+      selectedMembers.remove(user);
+      emit(SuccessDeleteMember());
+    } else {
+      selectedMembers.add(user);
+      emit(SuccessAddMember());
+    }
+  }
+
+  void cleanMembers() {
+    selectedMembers = [];
+    emit(SelectGroupMembersInitial());
+  }
+
+}
