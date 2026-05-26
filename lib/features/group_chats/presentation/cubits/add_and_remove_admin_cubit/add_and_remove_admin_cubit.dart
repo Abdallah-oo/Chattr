7747e7344ca210fa56817fbdf365ada3d6e9267d
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-05-26 15:53:46 +0300

    feat: implement admin management cubit

---
+++ lib/features/group_chats/presentation/cubits/add_and_remove_admin_cubit/add_and_remove_admin_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:messenger_clone0/features/group_chats/data/repos/add_and_remove_admin_repo/add_and_remove_admin_repo.dart';
+part 'add_and_remove_admin_state.dart';
+
+class AddAndRemoveAdminCubit extends Cubit<AddAndRemoveAdminState> {
+  AddAndRemoveAdminCubit({
+    required AddAndRemoveAdminRepo repo,
+  }) : _repo = repo,
+       super(AddAndRemoveAdminInitial());
+
+  final AddAndRemoveAdminRepo _repo;
+  int? locaIndex;
+  Future<void> addAdminAndRemove({
+    required String groupId,
+    required String userId,
+    required bool isAdmin,
+  }) async {
+    emit(AddAndRemoveAdminLoading());
+    final result = await _repo.addAdminAndRemove(
+      groupId: groupId,
+      userId: userId,
+      isAdmin: isAdmin,
+    );
+    result.fold(
+      (e) => emit(AddAndRemoveAdminFailure(errorMessage: "$e")),
+      (u) => emit(AddAndRemoveAdminSuccess()),
+    );
+  }
+}
