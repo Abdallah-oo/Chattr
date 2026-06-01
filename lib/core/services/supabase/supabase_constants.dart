ba334d999155a6227bcc4340baf84a3028c85149
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-06-01 20:53:59 +0300

    stop tracking supabase constants

--- lib/core/services/supabase/supabase_constants.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseConstants {
-  static const String url = "https://xqjsecxgmnafmcarqjgt.supabase.co";
-  static const String anonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxanNlY3hnbW5hZm1jYXJxamd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwMDkxODksImV4cCI6MjA4MzU4NTE4OX0.Sk0u1eIr8drfxXkCqYH7qQ8UJ1IEXMuFsqeyR8pccr4";
-}
