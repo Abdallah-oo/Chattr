e43ea463be0fb16fcba1c1cb8ccd11f740790beb
Abdallah Ahmed <abdallah81786417@gmail.com>
2026-06-01 20:40:58 +0300

    stop tracking supabase constants

--- lib/core/services/supabase/supabase_constants.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseConstants {
-  static const String url = "https://xqjsecxgmnafmcarqjgt.supabase.co";
-  static const String publishKey =
-      "sb_publishable_wUHCQcb8fBtQMPb18FJzqg_s0Neq7fh";
-}
